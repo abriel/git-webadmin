4cc8acacb32233b5029871449a5f43c97edbbbc4
Dmitry Pisarev <spirit@live.1nsk.ru>
2011-04-28 00:14:18 +0700

    added model for user and his ssh keys.
    	modified:   core/models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class user(models.Model):
+	full_name  = models.CharField(max_length=200)
+	email      = models.EmailField()
+
+	def __unicode__(self):
+		return self.full_name
+
+	def count_of_keys(self):
+		return self.ssh_keys_set.count()
+
+
+class ssh_keys(models.Model):
+	key        = models.TextField()
+	user_id    = models.ForeignKey(user)
+
