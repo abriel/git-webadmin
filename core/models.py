ec004c811ee641a9234312a815fe2bbb342dd445
Dmitry Pisarev <spirit@live.1nsk.ru>
2011-04-28 12:43:52 +0700

    added model for repository and tune naming of another models.
    	modified:   core/models.py

--- core/models.py
+++ core/models.py
@@ -13,17 +13,50 @@ class user(models.Model):
 
 
 class ssh_keys(models.Model):
+
+	class Meta:
+		verbose_name        = 'ssh key'
+
 	key        = models.TextField()
 	user_id    = models.ForeignKey(user)
 
 
+class repository_system(models.Model):
+
+	class Meta:
+		verbose_name        = 'Repository system'
+
+	system_path = models.CharField(
+								max_length=250, 
+								verbose_name='URL to admin repo', 
+								help_text='Example: file:///var/git/repositories/gitosis-admin.git or git@example.com:gitosis-admin.git'
+								)
+	access_key  = models.TextField(help_text='your private ssh key, which will be used for access to admin repository')
+	engine      = models.CharField(
+								max_length=50,
+								help_text='Engine can be gitosis or gitolite'
+								)
+	
+	def __unicode__(self):
+		return self.system_path
+
+
 class git_repository(models.Model):
-	name  = models.CharField(max_length=200)
+
+	class Meta:
+		verbose_name        = 'Git repository'
+		verbose_name_plural = 'Git repositories'
+
+	name       = models.CharField(max_length=200)
+	system     = models.ForeignKey(repository_system, verbose_name='repository system')
+	
+	def __unicode__(self):
+		return self.name + ' on ' + self.system.__unicode__()
 
 
 class access(models.Model):
 	repository = models.ForeignKey(git_repository)
 	user       = models.ForeignKey(user)
 	read_only  = models.BooleanField()
-	branch     = models.CharField(max_length=200, null=True)
+	branch     = models.CharField(max_length=200, null=True, blank=True)
 
