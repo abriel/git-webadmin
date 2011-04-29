5cb8ae5cb966b2f4bee126caeb5c196f3867b536
Dmitry Pisarev <spirit@live.1nsk.ru>
2011-04-29 13:16:55 +0700

    added description for rmall()
    	modified:   self_libs/useful_func.py

--- self_libs/useful_func.py
+++ self_libs/useful_func.py
@@ -1,5 +1,15 @@
 import os
 
+################################################################
+# Use: "python rmall.py directoryPath directoryPath..."
+# recursive directory tree deletion: removes all files and
+# directories at and below directoryPaths; recurs into subdirs
+# and removes parent dir last, because os.rmdir requires that
+# directory is empty; like a Unix "rm -rf directoryPath"
+################################################################
+# Taken from http://codeidol.com/python/python3/System-Examples-Directories/Deleting-Directory-Trees/
+################################################################
+
 def rmall(dirPath):                             # delete dirPath and below
 	namesHere = os.listdir(dirPath)
 	for name in namesHere:                      # remove all contents first
