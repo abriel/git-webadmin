297823c89a3654b4dcf34219fa7d386144f15c4d
Dmitry Pisarev <spirit@live.1nsk.ru>
2011-05-04 20:49:17 +0700

    open django.admin
    	modified:   settings.py
    	modified:   urls.py

--- settings.py
+++ settings.py
@@ -116,9 +116,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'core',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -161,3 +162,4 @@ LOGGING = {
         }
     }
 }
+
