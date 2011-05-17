e02160c76760b1be6307f72bc75f1823f26eee0e
Dmitry Pisarev <spirit@live.1nsk.ru>
2011-05-17 23:16:16 +0700

    models.access : refactoring function check_keys()
    	modified:   core/models.py

--- core/models.py
+++ core/models.py
@@ -249,9 +249,7 @@ class access(models.Model):
 		return self.user.short_name + ' on ' + self.repository.name + ' mode ' + wmode
 
 	def check_keys(self):
-		affected_repository_systems = Repository_System.objects.filter(git_repository__access=self)
-		for repository_system in affected_repository_systems:
-			self.user.write_key_file(repository_system, only_check=True)
+		self.user.write_key_file(self.repository.system, only_check=True)
 
 
 post_delete.connect(ssh_key_post_delete, sender=ssh_keys)
