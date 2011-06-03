890975f2cdc1da04453883143dd786f50371edfc
Dmitry Pisarev <spirit@live.1nsk.ru>
2011-06-03 18:24:18 +0700

    added simple view and template for url /
    	modified:   core/views.py
    	modified:   settings.py
    	new file:   templates/core/index.html
    	modified:   urls.py

--- core/views.py
+++ core/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from core.models import Repository_System
+from django.shortcuts import render_to_response
+
+def index(request):
+	srepo_list = Repository_System.objects.all()
+	return render_to_response('core/index.html', {'srepo_list': srepo_list})
+	
--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'gitolite_webadmin.views.home', name='home'),
+    url(r'^$', 'core.views.index'),
     # url(r'^gitolite_webadmin/', include('gitolite_webadmin.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
